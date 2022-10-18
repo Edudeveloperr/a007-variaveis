369d6a759b7fce12cb0462c76e5a94c7af27414d
Edudeveloperr <edudeveloperr@gmail.com>
2022-10-18 20:40:48 -0300

    Foi realizado os itens do exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+let nome
+let idade
+console.log("nome", typeof nome)
+console.log("idade", typeof idade)
+
+nome= prompt("Digite seu nome")
+idade= prompt("Digite sua idade")
+
+console.log("nome", typeof nome)
+console.log("idade", typeof idade)
+
+console.log("Olá", nome ,"Você tem", idade ,"anos.")
+
+let pergunta1= "Você ama tecnologia?"
+let pergunta2= "Você é programador?"
+let pergunta3= "Gosta de codar"
+
+pergunta1= prompt(pergunta1)
+pergunta2= prompt(pergunta2)
+pergunta3= prompt(pergunta3)
+
+
+console.log (pergunta1, ",eu amo tecnologia.", pergunta2, " ,sou programador por amor.", pergunta3, ", amo codar, acho fascinante.")
+
+
+
