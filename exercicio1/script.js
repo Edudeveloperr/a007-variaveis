d983c758e1b037440f8f150543386f7ee7161fcd
Edudeveloperr <edudeveloperr@gmail.com>
2022-10-20 20:06:25 -0300

    Adicionar exercicio 1 - a0007

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,11 @@
+//Parte 1
+
 let nome
 let idade
 console.log("nome", typeof nome)
 console.log("idade", typeof idade)
-
+/* O tipo da variavel foi undefined, pois declaramos, 
+mas nao atribuimos um valor a ela.*/
 nome= prompt("Digite seu nome")
 idade= prompt("Digite sua idade")
 
@@ -11,16 +14,15 @@ console.log("idade", typeof idade)
 
 console.log("Olá", nome ,"Você tem", idade ,"anos.")
 
-let pergunta1= "Você ama tecnologia?"
-let pergunta2= "Você é programador?"
-let pergunta3= "Gosta de codar"
+
+// Parte 2
 
 pergunta1= prompt(pergunta1)
 pergunta2= prompt(pergunta2)
 pergunta3= prompt(pergunta3)
 
+let pergunta1= "Você ama tecnologia?"
+let pergunta2= "Você é programador?"
+let pergunta3= "Gosta de codar"
 
 console.log (pergunta1, ",eu amo tecnologia.", pergunta2, " ,sou programador por amor.", pergunta3, ", amo codar, acho fascinante.")
-
-
-
