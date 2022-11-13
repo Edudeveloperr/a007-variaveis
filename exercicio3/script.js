7143661d5dd073531276ecb225aed739ad97c5c1
Edudeveloperr <edudeveloperr@gmail.com>
2022-11-13 19:04:22 -0300

    Conclusão Exercicio 3

---
+++ exercicio3/script.js
@@ -0,0 +1,56 @@
+let nome
+nome = prompt("Digite seu Nome completo:")
+console.log(nome)
+
+let dataNasc
+dataNasc = prompt("Digite sua data de Nascimento:")
+console.log(dataNasc)
+
+let endereco
+endereco = prompt("Digite seu Endereço:")
+console.log(dataNasc)
+
+let cpf
+cpf = prompt("Digite seu CPF")
+console.log(cpf)
+
+let cnh
+cnh = prompt("`Possui CNH, Sim ou Não?")
+console.log(cnh)
+
+let qtdFilhos
+qtdFilhos = prompt("Possui quanto filhos?")
+console.log(qtdFilhos)
+
+let cargo
+cargo = prompt("Qual seu Cargo")
+console.log(cargo)
+
+let salario 
+salario = prompt("Qual seu salario?")
+console.log(salario)
+
+let comissao
+comissao = prompt("Recebe Comissão? Se sim, informe a porcentagem, se não, digite 0")
+let porcentagem
+porcentagem = prompt("Qual a porcentagem da comissão, caso tenha:")
+console.log(comissao)
+console.log(porcentagem)
+
+let anoAdmissao
+anoAdmissao = prompt("Informe o ano em que foi admitido na empresa:")
+console.log(anoAdmissao)
+
+console.log("Nome", typeof nome)
+console.log("Data de Nascimento", typeof dataNasc)
+console.log("Endereço", typeof endereco)
+console.log("CPF", typeof cpf)
+console.log("Escolaridade", typeof Escolaridade)
+console.log("CNH", typeof cnh)
+console.log("Quantos filhos", typeof qtdFilhos)
+console.log("Cargo", typeof cargo)
+console.log("Salario", typeof salario)
+console.log("Comissão", typeof comissao)
+console.log("Ano de admissão", typeof anoAdmissao)
+
+
